079b65a2f1cc38abfa9e32a246a371909365a5db
Alexandra Sazhaeva <alexa.sazhaeva@gmail.com>
2023-07-19 00:24:08 +0200

    Created get_service and to_join functions

--- src/channel.py
+++ src/channel.py
@@ -1,19 +1,22 @@
 import os
+import json
 from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб - канала"""
-    api_key: str = os.getenv('YOUTUBE_API_KEY')
+    API_KEY: str = os.getenv('YOUTUBE_API_KEY')
+    DATA_FILE = "/home/shurochka/PycharmProjects/youtube-analytics-project2/data.json"
+    YOUTYBE = build('youtube', 'v3', developerKey=API_KEY)
 
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала."""
 
         # Получаем данные в виде словаря
-        youtube = build('youtube', 'v3', developerKey=Channel.api_key)
-        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        channel = Channel.YOUTYBE.channels().list(id=channel_id, part='snippet,statistics').execute()
 
+        # Вычленяем необходимую информацию
         self.channel_id = channel_id
         self.title = channel["items"][0]["snippet"]["title"]
         self.description = channel["items"][0]["snippet"]["description"]
@@ -25,7 +28,36 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        youtube = build('youtube', 'v3', developerKey = Channel.api_key)
-        channel = youtube.channels().list(id = self.channel_id,
+        channel = Channel.YOUTYBE.channels().list(id = self.channel_id,
         part = 'snippet,statistics').execute()
         print(channel)
+
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API"""
+        return cls.YOUTYBE
+
+
+    def to_json(self) -> None:
+        """
+        Сохраняет в файл значения атрибутов экземпляра Channel
+        """
+
+        # Записываем необходимые данные в словарь
+        dictionary = {"channel_id": self.channel_id, "title": self.title,
+                      "description": self.description, "link": self.link,
+                      "number_of_subscribers": self.number_of_subscribers,
+                      "number_of_video": self.number_of_video,
+                      "number_of_views": self.number_of_views}
+
+        # Записываем данные в json файл
+        if os.stat(Channel.DATA_FILE).st_size == 0:
+            with open(Channel.DATA_FILE, "w", encoding="utf-8") as f:
+                json.dump([dictionary], f, ensure_ascii=False)
+        else:
+            with open(Channel.DATA_FILE, "r", encoding="utf-8") as f:
+                data_list = json.load(f)
+            data_list.append(dictionary)
+            with open(Channel.DATA_FILE, "w", encoding="utf-8") as f:
+                json.dump(data_list, f, ensure_ascii=False)
