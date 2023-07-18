f1eded2025a5c2b7aa103989c1b7d9c62da775d0
Alexandra Sazhaeva <alexa.sazhaeva@gmail.com>
2023-07-18 21:18:49 +0200

    Updated the constructor

--- src/channel.py
+++ src/channel.py
@@ -9,7 +9,18 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала."""
+
+        # Получаем данные в виде словаря
+        youtube = build('youtube', 'v3', developerKey=Channel.api_key)
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
         self.channel_id = channel_id
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.link = channel["items"][0]["snippet"]["customUrl"]
+        self.number_of_subscribers = channel["items"][0]["statistics"]["subscriberCount"]
+        self.number_of_video = channel["items"][0]["statistics"]["videoCount"]
+        self.number_of_views = channel["items"][0]["statistics"]["viewCount"]
 
 
     def print_info(self) -> None:
