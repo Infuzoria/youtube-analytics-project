9ba8cf924dec6f19a7b07f6f4c7377849c8f06c2
Alexandra Sazhaeva <alexa.sazhaeva@gmail.com>
2023-08-06 18:37:15 +0200

    Wrote show_best_video function

--- src/playlist.py
+++ src/playlist.py
@@ -2,7 +2,7 @@ import os
 import datetime
 import isodate
 from googleapiclient.discovery import build
-
+from src.video import Video
 
 class PlayList:
     """Класс для получения информации о плейлисте"""
@@ -12,13 +12,18 @@ class PlayList:
     def __init__(self, playlist_id: str) -> None:
         """Конструктор класса"""
 
-        # Получаем информацию в виде словаря
-        playlist = PlayList.YOUTYBE.playlistItems().list(playlistId=playlist_id,
-                                                         part='snippet,contentDetails,id,status',
-                                                         maxResults=50).execute()
+        # Получаем информацию в виде словаря (необходимо для получения названия плейлиста)
+        self.playlist_info = PlayList.YOUTYBE.playlists().list(id=playlist_id,
+                                                               part='snippet,contentDetails',
+                                                               maxResults=50).execute()
+
+        # Получаем информацию в виде словаря (необходимо для получения информации о видеороликах)
+        self.playlist = PlayList.YOUTYBE.playlistItems().list(playlistId=playlist_id,
+                                                              part='snippet,contentDetails,id,status',
+                                                              maxResults=50).execute()
 
         # Получаем id всех видеороликов
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist['items']]
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist['items']]
 
         # Получаем длительность всех видеороликов
         video_response = PlayList.YOUTYBE.videos().list(part='contentDetails,statistics',
@@ -31,7 +36,7 @@ class PlayList:
             duration = isodate.parse_duration(iso_8601_duration)
             delta += duration
 
-        self.title = playlist["items"][0]["snippet"]["title"]
+        self.title = self.playlist_info["items"][0]["snippet"]["title"]
         self.url = "https://www.youtube.com/playlist?list=" + playlist_id
         self.__total_duration = delta
 
@@ -39,6 +44,15 @@ class PlayList:
     def total_duration(self):
         return self.__total_duration
 
+    def show_best_video(self):
+        # Получаем id всех видеороликов
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist['items']]
 
-pl = PlayList("PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw")
-print(pl.total_duration)
+        # Ищем самое популярное видео
+        max_count_of_likes = 0
+        for row in video_ids:
+            video = Video(row)
+            if int(video.number_of_likes) > max_count_of_likes:
+                best_video = video
+        
+        return best_video.url
